a8936457943af9ce111d698d3e4973826d156efe
Iorvit <83587946+Iorvit@users.noreply.github.com>
2022-02-24 11:26:45 +0300

    Update eq_world_map.py

--- Data Visualization/eq_world_map.py
+++ Data Visualization/eq_world_map.py
@@ -12,7 +12,7 @@ filename = 'data/7erch.json'
 with open(filename, encoding='utf-8') as f:
     all_eq_data = json.load(f)
 
-# Созаёт файл в более удобочитаймом фомате.
+# Создаёт файл в более удобочитаймом фомате.
 """readable_file = 'data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
     json.dump(all_eq_data, f, indent=4)"""
